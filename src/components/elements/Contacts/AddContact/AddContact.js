d643ecdda600a1673048e156ba70356650fdd85f
Amitrajit31 <amitrajitdas31@gmail.com>
2021-05-13 22:51:35 +0530

    forms handled

--- src/components/elements/Contacts/AddContact/AddContact.js
+++ src/components/elements/Contacts/AddContact/AddContact.js
@@ -1,10 +1,10 @@
-import React from 'react';
+import React, { useState }  from 'react';
+
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import TextField from '@material-ui/core/TextField';
 import CardContent from '@material-ui/core/CardContent';
 import Grid from '@material-ui/core/Grid';
-import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 
 
@@ -14,17 +14,28 @@ const AddContact = () => {
 
     const classes = useStyles();
 
+    const [name,setName] = useState("");
+    const [phone,setPhone] = useState("");
+    const [email,setEmail] = useState("");
+
+    const CreateContact = e => {
+        e.preventDefault();
+        console.log(name, phone, email);
+    }
+
     return (
         <Grid container justify="center">
             <Card className={classes.root} variant="outlined">
                 <CardContent>
-                    <form className={classes.form} noValidate autoComplete="off">
+                    <form className={classes.form} onSubmit={(e) => CreateContact(e)}>
                         <Grid item xs={12}>
                             <TextField
                             required
                             id="name"
                             label="Name"
                             variant="outlined"
+                            value={name}
+                            onChange={(e) => setName(e.target.value)}
                             />
                         </Grid>
                         <Grid item xs={12}>
@@ -33,6 +44,8 @@ const AddContact = () => {
                             id="phone"
                             label="Phone Number"
                             variant="outlined"
+                            value={phone}
+                            onChange={(e) => setPhone(e.target.value)}
                             />
                         </Grid>
                         <Grid item xs={12}>
@@ -41,11 +54,13 @@ const AddContact = () => {
                             id="email"
                             label="E-mail"
                             variant="outlined"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
                             />
                         </Grid>
                         <Grid item xs={12}>
                             <CardActions>
-                                <Button className={classes.button}>Add</Button>
+                                <Button className={classes.button} type="submit">Add</Button>
                             </CardActions>
                                 
                         </Grid>
