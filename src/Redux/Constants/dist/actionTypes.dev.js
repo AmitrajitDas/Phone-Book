ef30cbac9fd796745289354f96692a697a587700
Amitrajit31 <amitrajitdas31@gmail.com>
2021-05-14 13:20:14 +0530

    type constants

---
+++ src/Redux/Constants/dist/actionTypes.dev.js
@@ -0,0 +1,12 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.DELETE = exports.UPDATE = exports.CREATE = void 0;
+var CREATE = 'CREATE';
+exports.CREATE = CREATE;
+var UPDATE = 'UPDATE';
+exports.UPDATE = UPDATE;
+var DELETE = 'DELETE';
+exports.DELETE = DELETE;
